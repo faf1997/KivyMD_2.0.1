a1be67c6676d043663f4125a2a3af265c421e7d0
faf1997 <alejandrofranfio@gmail.com>
2024-08-31 12:12:22 -0300

    clonado

--- kivymd/_version.py
+++ kivymd/_version.py
@@ -1,5 +1,5 @@
 release = False
 __version__ = "2.0.1.dev0"
-__hash__ = "f7bde69707ac708a758a02d89f14997ee468d1ee"
-__short_hash__ = "f7bde69"
-__date__ = "2024-02-27"
+__hash__ = "cfe5aac30f50708c6208c8b33b0ef1aa390721e8"
+__short_hash__ = "cfe5aac"
+__date__ = "2024-08-31"
